8cda1592002ee6b052959be1fb281fdf626d33fa
unknown <git@github.com:jmackitty/Testing.git>
2022-07-04 13:07:12 -0400

    Add module 13.4.2.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,17 +2,17 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437],4);
 
 
 //  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
+L.marker([34.0522, -118.2437], {
     radius: 300,
-    color: "black",
-    fillColor:"yellow"
+    color: "orange",
+    fillColor:"orange"
  }).addTo(map);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/satellite-v9',
 maxZoom: 18,
@@ -21,8 +21,19 @@ zoomOffset: -1,
 accessToken: API_KEY
 });
 
+// Get data from cities.js
+let cityData = cities;
 
-
+  
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
